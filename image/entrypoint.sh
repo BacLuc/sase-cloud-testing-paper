a996f0d41362dcb7bb31fffd620cec7de68f0214
BacLuc <lucius.bachmann@gmx.ch>
2023-03-12 15:38:24 +0100

    setup template

---
+++ image/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+set -e
+if [ "$1" = "default" ]; then
+  pdflatex_path=$(which pdflatex)
+  cmd="$pdflatex_path -output-format=pdf -halt-on-error"
+  cmd="$cmd -interaction=nonstopmode"
+  cmd="$cmd -output-directory=/out ${LATEX_MAIN_FILE}"
+  exec $cmd
+fi
+
+if [ "$1" = "format" ]; then
+  find . -name '*.tex' | xargs latexindent -w -s
+  find . -name '*.bak*' | xargs rm -f
+  find . -name '*indent.log*' | xargs rm -f
+  exit 0
+fi
+
+if [ "$1" = "check-format" ]; then
+  set +e
+  failed=0
+  for file in $(find . -name "*.tex"); do
+    latexindent -k $file
+    exitcode=$#
+    if [ $exitcode -gt 0 ]; then
+      failed=$exitcode
+    fi
+  done
+  find . -name '*indent.log*' | xargs rm -f
+  exit $failed
+fi
+
+exec "$@"
