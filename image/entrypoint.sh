93e88a819d4f6394b74ed218a0ff4f23f76d7fac
BacLuc <lucius.bachmann@gmx.ch>
2023-03-30 21:58:39 +0200

    ci: fix build and format

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-sudo chmod -R +w /workspace
-sudo chmod -R +w /out
-
 if [ "$1" = "default" ]; then
   bibtex_path=$(which bibtex)
   cp -a /workspace/* /out
@@ -21,25 +18,28 @@ if [ "$1" = "default" ]; then
 fi
 
 if [ "$1" = "format" ]; then
-  find . -name '*.tex' | xargs latexindent -w -s
-  find . -name '*.bak*' | xargs rm -f
-  find . -name '*indent.log*' | xargs rm -f
+  set -x
+  find . -name '*.tex' -print0 | xargs --null latexindent -w -s
+  find . -name '*.bak*' -print0 | xargs --null rm -f
+  find . -name '*indent.log*' -print0 | xargs --null rm -f
   exit 0
 fi
 
 if [ "$1" = "check-format" ]; then
   set +e
+  set -x
   failed=0
-  for file in $(find . -name "*.tex"); do
+  # shellcheck disable=SC2044
+  for file in $(find . -name "*.tex" -print0 | xargs --null echo); do
     latexindent $file > $file.formatted
     git diff --no-index --exit-code $file $file.formatted
-    exitcode=$#
+    exitcode=$?
     rm $file.formatted
     if [ $exitcode -gt 0 ]; then
       failed=$exitcode
     fi
   done
-  find . -name '*indent.log*' | xargs rm -f
+  find . -name '*indent.log*' -print0 | xargs --null rm -f
   exit $failed
 fi
 
